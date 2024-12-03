6b1ee2e7268f2a7d9709f966ba2e2509cd94d3ab
Daniel Amado <daniel.amadove@gmail.com>
2024-12-03 17:04:37 -0500

    Actualizar mensaje de WhatsApp para incluir interés en patrocinar líderes y proponer reunión

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export const URL_BASE = '/l/incubadora-de-lideres';
 const WHATSAPP = '573017807192';
-let TEXT = 'Hola, me gustaría saber más sobre la Incubadora de Líderes.';
+let TEXT = `Hola Leidy!!
+¡¡Me encanta el programa!!
+Quiero patrocinar lideres que luego sean embajadores de mi marca, 
+¿Nos reunimos a revisar?`;
 TEXT = encodeURI(TEXT);
 export const URL_WHATSAPP = `https://api.whatsapp.com/send?phone=${WHATSAPP}&text=${TEXT}`;
