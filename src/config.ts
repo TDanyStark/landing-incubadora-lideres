a2ecc8ebdcce7fcb3c2732d6af873a4fa37383b0
Daniel Amado <daniel.amadove@gmail.com>
2024-12-03 16:51:17 -0500

    Agregar secciones "Ayuda" y "Footer"; actualizar enlaces de WhatsApp y metadatos en Layout

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,5 @@
 export const URL_BASE = '/l/incubadora-de-lideres';
+const WHATSAPP = '573017807192';
+let TEXT = 'Hola, me gustaría saber más sobre la Incubadora de Líderes.';
+TEXT = encodeURI(TEXT);
+export const URL_WHATSAPP = `https://api.whatsapp.com/send?phone=${WHATSAPP}&text=${TEXT}`;
